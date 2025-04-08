e7552ef425e7c8142087449a9554851f7dffa85a
aantonpan <aantonpan@uoc.edu>
2025-04-08 20:16:30 +0200

    Arreglo add-player-form

--- src/app/components/players/add-player-form/add-player-form.component.ts
+++ src/app/components/players/add-player-form/add-player-form.component.ts
@@ -29,15 +29,49 @@ export class AddPlayerFormComponent {
     videoFile: '',
   };
 
-  // 🔽 Arxius disponibles a assets/img i assets/media
+  // Arxius disponibles a assets/img i assets/media
   availableImages: string[] = [
     'lebron.png',
     'tatum.png',
     'terry.png',
     'davis.png',
+    'brown.png',
+    'bruzelis.png',
+    'doncic.png',
+    'embiid.png',
+    'giannis.png',
+    'hauser.png',
+    'klay.png',
+    'miller.png',
+    'moody.png',
+    'norris.png',
+    'simmons.png',
+    'tatum.png',
+    'wemby.png',
+    'williams.png'
   ];
 
-  availableVideos: string[] = ['lebron.mp4', 'terry.mp4', 'video1.mp4'];
+  availableVideos: string[] = [
+    'terry.mp4',
+    'video1.mp4',
+    'buzelis.mp4',
+    'hauser.mp4',
+    'miler.mp4',
+    'williams.mp4',
+    'norris.mp4',
+    'video1.mp4',
+    'video2.mp4',
+    'video3.mp4',
+    'video4.mp4',
+    'video5.mp4',
+    'video6.mp4',
+    'video7.mp4',
+    'video8.mp4',
+    'video9.mp4',
+    'video10.mp4',
+    'video11.mp4',
+    'video12.mp4'
+  ];
 
   selectedImageName: string = '';
   selectedVideoName: string = '';
